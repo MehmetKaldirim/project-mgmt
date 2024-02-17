5c0c1fb4b1f3f5044b5760e4c543c7f3b320581f
MehmetKaldirim <mathdenizi@gmail.com>
2024-02-17 17:35:55 +0100

    Validation added to task using modal with portal

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,29 +1,49 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import Modal from "./Modal";
+
 export default function NewTask({ onAdd }) {
-  const [enteredTask, setEnteredTask] = useState('');
+  const modal = useRef();
+  const [enteredTask, setEnteredTask] = useState("");
 
   function handleChange(event) {
     setEnteredTask(event.target.value);
   }
   function handleClick() {
+    if (
+        enteredTask.trim() === "" 
+      ) {
+        modal.current.open();
+        return;
+      }
     onAdd(enteredTask);
     setEnteredTask("");
   }
   return (
-    <div className="flex items-center gap-4">
-      <input
-        type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-slate-200"
-        onChange={handleChange}
-        value={enteredTask}
-      />
-      <button
-        className="text-stone-600 hover:text-stone-950"
-        onClick={handleClick}
-      >
-        {" "}
-        Add Task
-      </button>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-800 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops... looks like you forgot to enter a value
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field
+        </p>
+      </Modal>
+      <div className="flex items-center gap-4">
+        <input
+          type="text"
+          className="w-64 px-2 py-1 rounded-sm bg-slate-200"
+          onChange={handleChange}
+          value={enteredTask}
+        />
+        <button
+          className="text-stone-600 hover:text-stone-950"
+          onClick={handleClick}
+        >
+          {" "}
+          Add Task
+        </button>
+      </div>
+    </>
   );
 }
